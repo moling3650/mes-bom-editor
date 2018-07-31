b37abb7b6f86cb25dca3945fe7f25dcaa27b2968
moling <365024424@qq.com>
2018-07-31 14:51:11 +0800

    重整页面构架

---
+++ src/components/BomEditor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="bom_editor">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BomEditor',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
