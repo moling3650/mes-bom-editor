65c2c7e27ed6622826815d1204c31e3061b1d608
moling <365024424@qq.com>
2018-07-31 18:51:34 +0800

    axios添加拦截器

--- src/apis/request.js
+++ src/apis/request.js
@@ -4,4 +4,12 @@ const request = axios.create({
   timeout: 5000
 })
 
+request.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response.data
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
+
 export default request
